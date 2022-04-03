c6c52c0ed30cde276a9fabfaf5be843cfa27cc35
asadulR <asad1252001@gmail.com.com>
2022-04-03 21:10:03 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Hello baby</h2>
+
+      <Routes>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
