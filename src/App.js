b195b00b0acc63be8e0a32fb57105bc824623211
asadulR <asad1252001@gmail.com.com>
2022-04-04 08:10:22 +0600

    footer and 404 page added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './components/Blogs/Blogs';
 import DashBoard from './components/DashBoard/DashBoard';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 import AllReviews from './components/Reviews/AllReviews';
 
 function App() {
@@ -16,7 +18,10 @@ function App() {
         <Route path='/reviews' element ={<AllReviews/>}></Route>
         <Route path='/dashboard' element ={<DashBoard/>}></Route>
         <Route path='/blogs' element ={<Blogs/>}></Route>
+        <Route path='*' element ={<NotFound/>}></Route>
+
       </Routes>
+      <Footer/>
     </div>
   );
 }
